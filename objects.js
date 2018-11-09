6b1550e70e5ccea5def8183e2caa03ebc2fa871b
Anthony Mai <adonksmasher@gmail.com>
2018-11-09 00:49:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,7 +8,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   
 }
 
-function removeFromPlaylist() {
+function removeFromPlaylist(playlist,artistName) {
   delete playlist.artistName
   return playlist
 }
